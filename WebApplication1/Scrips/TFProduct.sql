801e49e03ac94f77bc27c55cd4f497d76d53202d
Marc Schwägerl <marc.schwaegerl@alumni.fh-aachen.de>
2023-07-19 02:48:51 +0200

    scripts

---
+++ WebApplication1/Scrips/TFProduct.sql
@@ -0,0 +1,101 @@
+﻿
+IF NOT EXISTS(SELECT 1 FROM sys.tables WHERE name = 'TFProduct')
+BEGIN
+	CREATE TABLE TFProduct(
+		Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+		Term NVARCHAR(100) NOT NULL,
+		rawCount INT  NULL,
+		TermFrequency REAL NULL,
+		logNormalized REAL NULL,
+	);
+INSERT INTO TfProduct(Term)
+ SELECT DISTINCT value
+    FROM CPEs  
+        CROSS APPLY STRING_SPLIT(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(
+							Product, ' ', '_'), ',', '_'), '.', '_'),
+									':', '_'), '\t', '_'), '\', '_'),
+									'!', '_'), '"', '_'),'/', '_'),
+									'#', '_'), '&', '_'),'%', '_'),
+									'$', '_'), '?', '_'),'@', '_'),
+									'(', '_'), ')', '_'),'[', '_'),
+									']', '_'), '+', '_'),'|', '_'),
+									'*', '_'), '<', '_'),'>', '_'),
+									'=', '_'), '^', '_'),'-', '_'),
+									'''','_'),
+							'_')
+    WHERE value != ''
+	ORDER BY value ASC
+END
+
+UPDATE TFProduct
+SET rawCount =
+(   SELECT COUNT(1)
+    FROM 
+	(SELECT DISTINCT Product FROM CPEs)t
+        CROSS APPLY STRING_SPLIT(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(
+							t.Product, ' ', '_'), ',', '_'), '.', '_'),
+									':', '_'), '\t', '_'), '\', '_'),
+									'!', '_'), '"', '_'),'/', '_'),
+									'#', '_'), '&', '_'),'%', '_'),
+									'$', '_'), '?', '_'),'@', '_'),
+									'(', '_'), ')', '_'),'[', '_'),
+									']', '_'), '+', '_'),'|', '_'),
+									'*', '_'), '<', '_'),'>', '_'),
+									'=', '_'), '^', '_'),'-', '_'),
+									'''','_'),
+							'_')
+ WHERE value = Term)
+ UPDATE TFProduct
+SET
+TermFrequency = rawCount / @totalTermCount,
+logNormalized = log(1+rawCount)
+
+
+
+DECLARE @totalTermCount INT
+SET @totalTermCount = (SELECT COUNT(value)
+    FROM 
+	(SELECT DISTINCT Product FROM CPEs)t
+        CROSS APPLY STRING_SPLIT(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(REPLACE(REPLACE(
+						REPLACE(
+							t.Product, ' ', '_'), ',', '_'), '.', '_'),
+									':', '_'), '\t', '_'), '\', '_'),
+									'!', '_'), '"', '_'),'/', '_'),
+									'#', '_'), '&', '_'),'%', '_'),
+									'$', '_'), '?', '_'),'@', '_'),
+									'(', '_'), ')', '_'),'[', '_'),
+									']', '_'), '+', '_'),'|', '_'),
+									'*', '_'), '<', '_'),'>', '_'),
+									'=', '_'), '^', '_'),'-', '_'),
+									'''','_'),
+							'_') WHERE value IS NOT NULL AND value != '')
+GO
---
+++ WebApplication1/Scrips/getAllResults - Copy.sql
@@ -0,0 +1,9 @@
+﻿DECLARE @t TABLE (name VARCHAR(100),truePos INT, trueNeg INT, falPos INT, falNeg INT, Spec REAL, Recall REAL, Precision REAL, Accuracy REAL, f1 REAL)
+INSERT INTO @t EXEC	 [dbo].[EvaluateResultsProduct] @typ = 1
+INSERT INTO @t 	EXEC [dbo].[EvaluateResultsProduct] @typ = 2
+INSERT INTO @t 	EXEC [dbo].[EvaluateResultsProduct] @typ =3
+INSERT INTO @t 	EXEC [dbo].[EvaluateResultsVersion] @typ = 1
+INSERT INTO @t 	EXEC [dbo].[EvaluateResultsVersion] @typ = 2
+INSERT INTO @t 	EXEC [dbo].[EvaluateResultsVersion] @typ = 3
+
+SELECT * FROM @t

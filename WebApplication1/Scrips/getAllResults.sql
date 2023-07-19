6c31b3e2adc75860cb3880aefde2bd31ddae52d4
Marc Schwägerl <marc.schwaegerl@alumni.fh-aachen.de>
2023-07-20 01:10:54 +0200

    dsa

--- WebApplication1/Scrips/getAllResults.sql
+++ WebApplication1/Scrips/getAllResults.sql
@@ -7,3 +7,13 @@ INSERT INTO @t 	EXEC [dbo].[EvaluateResultsVersion] @typ = 2
 INSERT INTO @t 	EXEC [dbo].[EvaluateResultsVersion] @typ = 3
 
 SELECT * FROM @t
+
+DECLARE @g TABLE (name VARCHAR(100), avgTruPos REAL, maxTruePos REAL, minTruePos REAL, avgFalsePos REAL, maxFalsePos REAL, minFalsePos REAL)
+INSERT INTO @g EXEC	 EvaluateGlobalScoreProduct @typ = 1
+INSERT INTO @g 	EXEC EvaluateGlobalScoreProduct @typ = 2
+INSERT INTO @g 	EXEC EvaluateGlobalScoreProduct @typ =3
+INSERT INTO @g 	EXEC EvaluateGlobalScoreVersion @typ = 1
+INSERT INTO @g 	EXEC EvaluateGlobalScoreVersion @typ = 2
+INSERT INTO @g 	EXEC EvaluateGlobalScoreVersion @typ = 3
+
+SELECT * FROM @g

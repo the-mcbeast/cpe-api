1cd4be6c16e8b2d12a0e55127b0a62c4e2e8a2f3
Marc Schwägerl <marc.schwaegerl@alumni.fh-aachen.de>
2023-07-20 03:56:34 +0200

    de

--- WebApplication1/Scrips/CalcNrWithSpacing.sql
+++ WebApplication1/Scrips/CalcNrWithSpacing.sql
@@ -1,6 +1,13 @@
-﻿SELECT COUNT(*) FROM (SELECT DISTINCT Vendor FROM CPEs WHERE Vendor  LIKE '%[_]%')b
-SELECT COUNT(*) FROM (SELECT DISTINCT Vendor FROM CPEs)c
+﻿SELECT *, (seperatedVendors*1.0/allVendors)*100 AS percentileVendors,
+(seperatedProducts*1.0/allProducts)*100 AS percentileProducts FROM
+(
+SELECT
+(SELECT COUNT(*) FROM (SELECT DISTINCT Vendor FROM CPEs WHERE Vendor  LIKE '%[_]%')b)AS seperatedVendors,
+(SELECT COUNT(*) FROM (SELECT DISTINCT Vendor FROM CPEs)c) As allVendors,
 
-SELECT COUNT(*) FROM (SELECT DISTINCT Product FROM CPEs WHERE Product  LIKE '%[_]%')b
-SELECT COUNT(*) FROM (SELECT DISTINCT Product FROM CPEs)c
+(SELECT COUNT(*) FROM (SELECT DISTINCT Product FROM CPEs WHERE Product  LIKE '%[_]%')b) AS seperatedProducts,
+(SELECT COUNT(*) FROM (SELECT DISTINCT Product FROM CPEs)c) As allProducts )b 
 
+
+seperatedVendors	allVendors	seperatedProducts	allProducts	percentileVendors	percentileProducts
+5379	15422	58156	85169	34.878744650400	68.283060738000
---
+++ WebApplication1/Scrips/VersionsWithDot.sql
@@ -0,0 +1,3 @@
+﻿SELECT COUNT(*) AS SemanticSeperatedVersions FROM Anfragen WHERE Id < 132 and Version like '%.%' 
+
+50
